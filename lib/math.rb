556ac730a6e0253585861004c9140e045598405e
delightful-tree-6762 <mduckworth@gmail.com>
2019-02-09 22:27:41 +0000

    Done.

--- lib/math.rb
+++ lib/math.rb
@@ -1,21 +1,28 @@
-def addition(num1, num2)
+def addition(num1 + num2)
+  puts (6, 17)
 end
 
-def subtraction(num1, num2)
+def subtraction(num1 - num2)
+  puts (94, 22)
 end
 
-def division(num1, num2)
+def division(num1 / num2)
+puts (25, 5)
 end
 
-def multiplication(num1, num2)
+def multiplication(num1 * num2)
+  puts (3, 5)
 end
 
-def modulo(num1, num2)
+def modulo(num1 % num2)
+  puts (11< 7)
 end
 
 def square_root(num)
+  puts Math.sqrt(9)
 end
 
 def order_of_operation(num1, num2, num3, num4)
   #Hint:  __ + (( __ * __ ) / __ )
+  puts 98 + (59/(13*8)) * -5
 end
